2886539cb8089a0a799ca849a2d018f1f6d1f7a2
gadielkalleb <gadiel_kalleb@hotmail.com>
2018-04-11 14:42:50 -0300

    criação do comportamento do component btnEnviar

--- src/components/BtnEnviar.js
+++ src/components/BtnEnviar.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import { Button, Icon, Row, Col } from 'react-materialize'
+import React, {Component} from 'react'
+import { Button, Icon, Row, Col, Input } from 'react-materialize'
 
-const BtnEnviar = props => 
-    <Row className="container">
-        <Col s={12} className="right-align">
-            <Button waves='light' >Enviar<Icon right>send</Icon></Button>
-        </Col>
-    </Row>
+class BtnEnviar extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            btnDisable: true,
+            checkboxState: false
+        }
+    }
+
+    handleChange(event){
+        console.log(event.target.value)
+    }
+
+    componentDidMount(){
+
+    }
+        
+    render(){
+        
+        return (
+            <Row className="container">
+                <Input name='confirma' type='checkbox' value={this.event} onChange={this.handleChange} label='Clique aqui para confirma seu pedido antes de clicar no botão enviar'/>
+                <Col s={12} className="right-align">
+                    <Button waves='light' disabled={this.state.btnDisable}>Enviar<Icon right>send</Icon></Button>
+                </Col>
+            </Row>
+        )
+    } 
+} 
+    
 
 
 export default BtnEnviar
