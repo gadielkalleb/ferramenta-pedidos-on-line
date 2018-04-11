80d66627e62e157c91d0de2c18eb65f7ba57ab4d
gadielkalleb <gadiel_kalleb@hotmail.com>
2018-04-10 21:26:36 -0300

    alteração dos dados das tabelas

---
+++ src/tabelas/produtos/aluSemRebaixo.js
@@ -0,0 +1,16 @@
+const kc4pcanto = {
+    'kc4pcanto300': {
+        tipo: 'KC 4P CANTO',
+        tamanho: '1,00',
+        codblindex: '300',
+        cod_sbm_blindex_FF: 'CodBlindex_300FF_CodSBM_21270',
+        cod_sbm_blindex_PB: 'CodBlindex_300PB_CodSBM_21271',
+        cod_sbm_blindex_BF: 'CodBlindex_300BF_CodSBM_21272',
+        cod_sbm_blindex_DB: 'CodBlindex_300DB_CodSBM_22301',
+        cod_sbm_blindex_BC: 'CodBlindex_300BC_CodSBM_21273',
+        cod_sbm_blindex_PT: 'CodBlindex_300PT_CodSBM_21274',
+        cod_sbm_blindex_BG: 'CodBlindex_300BG_CodSBM_21275'
+        
+    },
+
+}
